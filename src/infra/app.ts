e99164adf6ab9b9644d80fba0665df7362811e08
jefferson.silva <jefferson_1996Th@hotmail.com>
2022-11-14 19:12:28 -0300

    git add settings and port

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -1,12 +1,11 @@
 import express from "express";
 import { attachControllers } from "@decorators/express";
 import { HelloWolrd } from "~/src/domain/controllers";
-
+import settings from "./config/settings";
 const app = express();
-const port = 8080;
 
 attachControllers(app, [HelloWolrd]);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port 2 ${port}`);
+app.listen(settings.PORT, () => {
+  console.log(`app listening on port ${settings.PORT}`);
 });
